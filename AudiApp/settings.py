d21d775e20a6c84d2a6fd5658625ad6daefc704d
pmorenoq <pmorenoq@eafit.edu.co>
2025-03-03 18:06:11 -0500

    Feat: models & templates

--- AudiApp/settings.py
+++ AudiApp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'AudiAppApp.apps.AudiappappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- AudiAppApp/admin.py
+++ AudiAppApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Organizacion, Usuario
+
 # Register your models here.
+admin.site.register(Organizacion)
+admin.site.register(Usuario)
