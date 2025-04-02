b645ba66c5eb29b73c190d1d48d1fa0c5c54b425
pmorenoq <pmorenoq@eafit.edu.co>
2025-04-02 18:51:32 -0500

    fix: project structure main

--- AudiAppApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AudiappappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'AudiAppApp'
--- AudiAppApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import home
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path('home', home, name='home'),
-]
-
